705fea344c50d7b216e2cba9e24e000d2577e25b
Joseph Sortino <josephsortino@hotmail.com>
2020-03-06 14:54:12 -0500

    added more counters

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
+import Marquee from "react-text-marquee";
+
 import axios from "axios";
 import L from "leaflet";
 import HeatmapLayer from "react-leaflet-heatmap-layer";
@@ -9,29 +11,43 @@ import { isCompositeComponent } from "react-dom/test-utils";
 import { Navbar } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Logo from "./Biohazard.png";
+import Ticker, { FinancialTicker, NewsTicker } from "nice-react-ticker";
+
+var numeral = require("numeral");
 
 function App() {
   const style = {
     width: "100%",
-    height: "600px"
+    height: "400px"
   };
 
   const [data, setData] = useState([]);
   const [infected, setInfected] = useState("");
+  const [deaths, setDeaths] = useState("");
+  const [recovered, setRecovered] = useState("");
 
   useEffect(() => {
-    axios
-      .get("https://coronavirus-tracker-api.herokuapp.com/confirmed")
-      .then(res => {
-        const rawdata = res.data.locations;
-        const totalInfected = res.data.latest;
-        setInfected(totalInfected);
-        var result = [];
-        console.log(totalInfected);
-        const map1 = rawdata.map(data => data.coordinates);
-        for (var i in map1) result.push([map1[i].lat, map1[i].long]);
-        setData(result);
-      });
+    const urlQuery = ["confirmed", "deaths", "recovered"];
+
+    urlQuery.map((qstring, index) =>
+      axios
+        .get(`https://coronavirus-tracker-api.herokuapp.com/${qstring}`)
+        .then(res => {
+          if (qstring == "confirmed") {
+            const rawdata = res.data.locations;
+            setInfected(res.data.latest);
+            var result = [];
+            //console.log(totalInfected);
+            const map1 = rawdata.map(data => data.coordinates);
+            for (var i in map1) result.push([map1[i].lat, map1[i].long]);
+            setData(result);
+          } else if (qstring == "deaths") {
+            setDeaths(res.data.latest);
+          } else if (qstring == "recovered") {
+            setRecovered(res.data.latest);
+          }
+        })
+    );
   }, []);
 
   return (
@@ -69,7 +85,15 @@ function App() {
       <div class="flex-container">
         <div>
           <h4>Total Infected</h4>
-          {infected}
+          {numeral(infected).format("0,0")}
+        </div>
+        <div>
+          <h4>Total Deaths</h4>
+          {numeral(deaths).format("0,0")}
+        </div>
+        <div>
+          <h4>Total Recovered</h4>
+          {numeral(recovered).format("0,0")}
         </div>
       </div>
     </div>
